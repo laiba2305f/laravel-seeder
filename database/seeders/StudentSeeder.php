c20a2af467bae0ba4e4653e5da7b1e45b2be7f47
laiba2305f <laiba2305f@aptechsite.net>
2024-08-10 20:44:52 +0500

    first commit

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Student::create([
+             'stdname'=>'Seederuser',
+                "batchid"=>'2505E',
+                "course"=>'CPISM'
+        ]);
+         Student::Create(
+            [
+                'stdname'=>'Seederuser01',
+                "batchid"=>'2505E',
+                "course"=>'CPISM'
+            ]
+        );
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });  
+
+Route::resource('student','App\Http\Controllers\StudentController');
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
